2cce70af821121e5776f57cc79fd2d2095f7d5b5
rkw1316 <zio0816@gmail.com>
2019-11-17 23:45:29 +0900

    viewport 成功

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,16 @@
 import { configure, addParameters, addDecorator } from '@storybook/react';
 import { withOptions } from '@storybook/addon-options';
 import { withKnobs } from '@storybook/addon-knobs';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+addParameters({
+    viewport: {
+        viewports: {
+            ...INITIAL_VIEWPORTS
+        },
+        defaultViewport: 'iphonexr'
+    }
+});
 
 addDecorator(
     withKnobs({
